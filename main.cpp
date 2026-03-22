65fc9891272010abcf869e5baddbc88279a3c0e0
mohamedkhaled10105 <mohamedkhaled4343@gmail.com>
2026-03-22 15:03:44 +0000

    final testing and improved output format

---
+++ main.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_PROCESSES 20
+
+double fcfs(int arrival[], int burst[], int n) {
+    int currentTime = 0;
+    double totalWaiting = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (currentTime < arrival[i])
+            currentTime = arrival[i];
+
+        totalWaiting += (currentTime - arrival[i]);
+        currentTime += burst[i];
+    }
+
+    return totalWaiting / n;
+}
+
+double sjf(int arrival[], int burst[], int n) {
+    int done[MAX_PROCESSES] = {0};
+    int finished = 0;
+    int currentTime = 0;
+    double totalWaiting = 0;
+
+    while (finished < n) {
+        int index = -1;
+        int smallest = 100000;
+
+        for (int i = 0; i < n; i++) {
+            if (done[i] == 0 && arrival[i] <= currentTime && burst[i] < smallest) {
+                smallest = burst[i];
+                index = i;
+            }
+        }
+
+        if (index != -1) {
+            totalWaiting += (currentTime - arrival[index]);
+            currentTime += burst[index];
+            done[index] = 1;
+            finished++;
+        } else {
+            currentTime++;
+        }
+    }
+
+    return totalWaiting / n;
+}
+
+double roundRobin(int arrival[], int burst[], int n, int quantum) {
+    int remaining[MAX_PROCESSES];
+    int waiting[MAX_PROCESSES] = {0};
+    int lastExec[MAX_PROCESSES];
+
+    for (int i = 0; i < n; i++) {
+        remaining[i] = burst[i];
+        lastExec[i] = arrival[i];
+    }
+
+    int queue[MAX_PROCESSES];
+    int front = 0, rear = 0;
+    int inQueue[MAX_PROCESSES] = {0};
+
+    int currentTime = 0;
+
+    while (true) {
+        int done = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (remaining[i] > 0)
+                done = 0;
+        }
+
+        if (done == 1)
+            break;
+
+        for (int i = 0; i < n; i++) {
+            if (inQueue[i] == 0 && arrival[i] <= currentTime) {
+                queue[rear++] = i;
+                inQueue[i] = 1;
+            }
+        }
+
+        if (front < rear) {
+            int i = queue[front++];
+
+            waiting[i] += currentTime - lastExec[i];
+
+            int execTime;
+            if (remaining[i] < quantum) {
+                execTime = remaining[i];
+            } else {
+                execTime = quantum;
+            }
+
+            currentTime += execTime;
+            remaining[i] -= execTime;
+            lastExec[i] = currentTime;
+
+            for (int j = 0; j < n; j++) {
+                if (inQueue[j] == 0 && arrival[j] <= currentTime) {
+                    queue[rear++] = j;
+                    inQueue[j] = 1;
+                }
+            }
+
+            if (remaining[i] > 0) {
+                queue[rear++] = i;
+            }
+
+        } else {
+            currentTime++;
+        }
+    }
+
+    double totalWaiting = 0;
+    for (int i = 0; i < n; i++) {
+        totalWaiting += waiting[i];
+    }
+
+    return totalWaiting / n;
+}
+
+int main() {
+    int n = 5;
+
+    int arrival[MAX_PROCESSES] = {2, 0, 5, 1, 3};
+    int burst[MAX_PROCESSES]   = {6, 4, 2, 7, 3};
+
+    cout << "FCFS Average Waiting Time: " << fcfs(arrival, burst, n) << endl;
+    cout << "SJF Average Waiting Time: " << sjf(arrival, burst, n) << endl;
+    cout << "Round Robin Average Waiting Time: " << roundRobin(arrival, burst, n, 3) << endl;
+
+    return 0;
+} 
