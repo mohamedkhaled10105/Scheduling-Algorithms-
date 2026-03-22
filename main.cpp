3b4d4c096f1d8421c1170d1cc5ea17c06b8358f3
mohamedkhaled10105 <mohamedkhaled4343@gmail.com>
2026-03-22 14:24:31 +0000

    start program and test basic structure

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_PROCESSES 20
+
+int main() {
+    int n = 5;
+
+    int arrival[MAX_PROCESSES] = {2, 0, 5, 1, 3};
+    int burst[MAX_PROCESSES]   = {6, 4, 2, 7, 3};
+
+    cout << "Program started" << endl;
+
+    return 0;
+}
